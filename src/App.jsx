9378dcad2f2f3d0b2a8b5b5d38e2ca2e52e9680d
Eliezer Nsengi <avaranstudios@gmail.com>
2024-06-12 14:50:51 +0200

    feat: scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
-import { UserProfile } from "./components/UserProfile";
-export default function App() {
-  const callMe = () => console.log("hello world");
+// import { UserProfile } from "./components/UserProfile";
+// export default function App() {
+//   const callMe = () => console.log("hello world");
 
+//   return (
+//     <div>
+//       <h1>Root Component</h1>
+//       <UserProfile username="Nsengi" age={22} sex="Male" callMe={callMe} />
+//     </div>
+//   );
+// }
+
+export default function App() {
+  const mockUsers = [
+    {
+      id: 1,
+      username: "nsengi",
+      email: "nsengi@mail.com",
+    },
+    {
+      id: 2,
+      username: "richard",
+      email: "richard@mail.com",
+    },
+  ];
   return (
-    <div>
-      <h1>Root Component</h1>
-      <UserProfile username="Nsengi" age={22} sex="Male" callMe={callMe} />
-    </div>
+    <>
+      {mockUsers.map((user) => {
+        return (
+          <div key={user.id}>
+            <span>ID: {user.id} </span>
+            <span>
+              {" "}
+              <b>Username</b> : {user.username}{" "}
+            </span>
+            <span>
+              <b>Email</b>: {user.email}{" "}
+            </span>
+            <hr />
+          </div>
+        );
+      })}
+    </>
   );
 }
