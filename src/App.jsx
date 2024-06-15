4574f869fe12fb13ad26dda62eaa9efded0b3b74
Eliezer Nsengi <avaranstudios@gmail.com>
2024-06-15 17:03:51 +0200

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,57 @@
-import UserDetails from "./components/UserDetails";
+// import UserDetails from "./components/UserDetails";
 
+// export default function App() {
+//   const mockUsers = [
+//     {
+//       id: 1,
+//       username: "nsengi",
+//       email: "nsengi@mail.com",
+//     },
+//     {
+//       id: 2,
+//       username: "richard",
+//       email: "richard@mail.com",
+//     },
+//     {
+//       id: 3,
+//       username: "user3",
+//       email: "user3@mail.com",
+//     },
+//   ];
+//   return (
+//     <>
+//       {mockUsers.map((user) => {
+//         return <UserDetails key={user.id} user={user} />;
+//       })}
+//     </>
+//   );
+// }
+
+// export default function Ap() {
+//   const isAuthenticated = true;
+//   return isAuthenticated ? (
+//     <div>
+//       <h1>Welcome, User!</h1>
+//     </div>
+//   ) : (
+//     <div>
+//       <span> You are not logged in</span>
+//     </div>
+//   );
+// }
 export default function App() {
-  const mockUsers = [
-    {
-      id: 1,
-      username: "nsengi",
-      email: "nsengi@mail.com",
-    },
-    {
-      id: 2,
-      username: "richard",
-      email: "richard@mail.com",
-    },
-    {
-      id: 3,
-      username: "user3",
-      email: "user3@mail.com",
-    },
-  ];
-  return (
-    <>
-      {mockUsers.map((user) => {
-        return <UserDetails key={user.id} user={user} />;
-      })}
-    </>
-  );
+  const USER_STATUS = "NOT_VERIFIED";
+  if (USER_STATUS == "NOT_VERIFIED") {
+    return (
+      <div>
+        <span>You are not verified. Please verify your Email.</span>
+      </div>
+    );
+  } else if (USER_STATUS === "VERIFIED") {
+    return (
+      <div>
+        <span>You are verified. Congrats</span>
+      </div>
+    );
+  }
 }
