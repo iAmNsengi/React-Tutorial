75309d0e7d9a9b90bbe73c4bc3e8c3fdb934faa2
Eliezer Nsengi <avaranstudios@gmail.com>
2024-06-16 22:39:03 +0200

    feat: usestate

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -6,38 +6,47 @@ export default function RegisterForm() {
   const [displayName, setDisplayName] = useState("");
 
   return (
-    <form>
+    <>
+      <form>
+        <div>
+          <label htmlFor="username">Username</label>
+          <br />
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <br />
+          <label htmlFor="password">Password</label>
+          <br />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <label htmlFor="displayName">Display Name</label>
+          <br />
+          <input
+            type="text"
+            name="displayName"
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)}
+          />
+          <br />
+          <button>Register</button>
+        </div>
+      </form>
+
       <div>
-        <label htmlFor="username">Username</label>
+        <span>Username: {username}</span>
         <br />
-        <input
-          type="text"
-          name="username"
-          id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <br />
-        <label htmlFor="password">Password</label>
-        <br />
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br />
-        <label htmlFor="displayName">Display Name</label>
-        <br />
-        <input
-          type="text"
-          name="displayName"
-          value={displayName}
-          onChange={(e) => setDisplayName(e.target.value)}
-        />
-        <br />
-        <button>Register</button>
+        <span>Password : {password}</span>
       </div>
-    </form>
+    </>
   );
 }
+ 
