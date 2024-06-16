e45fb5f7bfe07b88b463256ec2e9f32faacfc81a
Eliezer Nsengi <avaranstudios@gmail.com>
2024-06-16 20:41:46 +0200

    feat: usestate

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,18 +1,40 @@
+import { useState } from "react";
+
 export default function RegisterForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+
   return (
     <form>
       <div>
         <label htmlFor="username">Username</label>
         <br />
-        <input type="text" name="username" id="username" />
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
         <br />
         <label htmlFor="password">Password</label>
         <br />
-        <input type="password" name="password" />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <br />
         <label htmlFor="displayName">Display Name</label>
         <br />
-        <input type="text" name="displayName" />
+        <input
+          type="text"
+          name="displayName"
+          value={displayName}
+          onChange={(e) => setDisplayName(e.target.value)}
+        />
         <br />
         <button>Register</button>
       </div>
